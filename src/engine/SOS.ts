8973a6aab0bb5fdc2cc2e56d3610f205b69b8b8d
CAOYUhhu <32864828+CAOYUhhu@users.noreply.github.com>
2022-02-08 14:11:32 +0800

    Update SOS.ts

--- src/engine/SOS.ts
+++ src/engine/SOS.ts
@@ -1,767 +1,100 @@
+//claim nft+transfer nft
 import { TransactionRequest } from "@ethersproject/abstract-provider";
 import { Base } from "./Base";
 import { isAddress, parseUnits } from "ethers/lib/utils";
 import { Contract, providers } from "ethers";
 
+
+
+
 const ERC20_ABI = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "address",
-        name: "account",
-        type: "address"
+        "internalType": "address",
+        "name": "sender",
+        "type": "address"
       },
       {
-        internalType: "address",
-        name: "minter_",
-        type: "address"
+        "internalType": "address",
+        "name": "recipient",
+        "type": "address"
       },
       {
-        internalType: "uint256",
-        name: "mintingAllowedAfter_",
-        type: "uint256"
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
       }
     ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor"
-  },
-  {
-    anonymous: false,
-    inputs: [
+    "name": "transferFrom",
+    "outputs": [
       {
-        indexed: true,
-        internalType: "address",
-        name: "owner",
-        type: "address"
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "spender",
-        type: "address"
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256"
+					"internalType": "bool",
+					"name": "",
+					"type": "bool"
       }
     ],
-    name: "Approval",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "delegator",
-        type: "address"
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "fromDelegate",
-        type: "address"
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "toDelegate",
-        type: "address"
-      }
-    ],
-    name: "DelegateChanged",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "delegate",
-        type: "address"
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "previousBalance",
-        type: "uint256"
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "newBalance",
-        type: "uint256"
-      }
-    ],
-    name: "DelegateVotesChanged",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: "address",
-        name: "minter",
-        type: "address"
-      },
-      {
-        indexed: false,
-        internalType: "address",
-        name: "newMinter",
-        type: "address"
-      }
-    ],
-    name: "MinterChanged",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "from",
-        type: "address"
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "to",
-        type: "address"
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256"
-      }
-    ],
-    name: "Transfer",
-    type: "event"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "DELEGATION_TYPEHASH",
-    outputs: [
-      {
-        internalType: "bytes32",
-        name: "",
-        type: "bytes32"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "DOMAIN_TYPEHASH",
-    outputs: [
-      {
-        internalType: "bytes32",
-        name: "",
-        type: "bytes32"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "PERMIT_TYPEHASH",
-    outputs: [
-      {
-        internalType: "bytes32",
-        name: "",
-        type: "bytes32"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "account",
-        type: "address"
-      },
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address"
-      }
-    ],
-    name: "allowance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address"
-      },
-      {
-        internalType: "uint256",
-        name: "rawAmount",
-        type: "uint256"
-      }
-    ],
-    name: "approve",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool"
-      }
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "account",
-        type: "address"
-      }
-    ],
-    name: "balanceOf",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      },
-      {
-        internalType: "uint32",
-        name: "",
-        type: "uint32"
-      }
-    ],
-    name: "checkpoints",
-    outputs: [
-      {
-        internalType: "uint32",
-        name: "fromBlock",
-        type: "uint32"
-      },
-      {
-        internalType: "uint96",
-        name: "votes",
-        type: "uint96"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "decimals",
-    outputs: [
-      {
-        internalType: "uint8",
-        name: "",
-        type: "uint8"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "delegatee",
-        type: "address"
-      }
-    ],
-    name: "delegate",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "delegatee",
-        type: "address"
-      },
-      {
-        internalType: "uint256",
-        name: "nonce",
-        type: "uint256"
-      },
-      {
-        internalType: "uint256",
-        name: "expiry",
-        type: "uint256"
-      },
-      {
-        internalType: "uint8",
-        name: "v",
-        type: "uint8"
-      },
-      {
-        internalType: "bytes32",
-        name: "r",
-        type: "bytes32"
-      },
-      {
-        internalType: "bytes32",
-        name: "s",
-        type: "bytes32"
-      }
-    ],
-    name: "delegateBySig",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      }
-    ],
-    name: "delegates",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "account",
-        type: "address"
-      }
-    ],
-    name: "getCurrentVotes",
-    outputs: [
-      {
-        internalType: "uint96",
-        name: "",
-        type: "uint96"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "account",
-        type: "address"
-      },
-      {
-        internalType: "uint256",
-        name: "blockNumber",
-        type: "uint256"
-      }
-    ],
-    name: "getPriorVotes",
-    outputs: [
-      {
-        internalType: "uint96",
-        name: "",
-        type: "uint96"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "minimumTimeBetweenMints",
-    outputs: [
-      {
-        internalType: "uint32",
-        name: "",
-        type: "uint32"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "dst",
-        type: "address"
-      },
-      {
-        internalType: "uint256",
-        name: "rawAmount",
-        type: "uint256"
-      }
-    ],
-    name: "mint",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "mintCap",
-    outputs: [
-      {
-        internalType: "uint8",
-        name: "",
-        type: "uint8"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "minter",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "mintingAllowedAfter",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "name",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      }
-    ],
-    name: "nonces",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      }
-    ],
-    name: "numCheckpoints",
-    outputs: [
-      {
-        internalType: "uint32",
-        name: "",
-        type: "uint32"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "owner",
-        type: "address"
-      },
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address"
-      },
-      {
-        internalType: "uint256",
-        name: "rawAmount",
-        type: "uint256"
-      },
-      {
-        internalType: "uint256",
-        name: "deadline",
-        type: "uint256"
-      },
-      {
-        internalType: "uint8",
-        name: "v",
-        type: "uint8"
-      },
-      {
-        internalType: "bytes32",
-        name: "r",
-        type: "bytes32"
-      },
-      {
-        internalType: "bytes32",
-        name: "s",
-        type: "bytes32"
-      }
-    ],
-    name: "permit",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "minter_",
-        type: "address"
-      }
-    ],
-    name: "setMinter",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "symbol",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "totalSupply",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256"
-      }
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "dst",
-        type: "address"
-      },
-      {
-        internalType: "uint256",
-        name: "rawAmount",
-        type: "uint256"
-      }
-    ],
-    name: "transfer",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool"
-      }
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        internalType: "address",
-        name: "src",
-        type: "address"
-      },
-      {
-        internalType: "address",
-        name: "dst",
-        type: "address"
-      },
-      {
-        internalType: "uint256",
-        name: "rawAmount",
-        type: "uint256"
-      }
-    ],
-    name: "transferFrom",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool"
-      }
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
+    "stateMutability": "nonpayable",
+    "type": "function"
   }
 ]
 
-const SOSCLAIM_ABI = [
+const cmbCLAIM_ABI = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint256",
-        name: "amountV",
-        type: "uint256"
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
       },
       {
-        internalType: "bytes32",
-        name: "r",
-        type: "bytes32"
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
       },
       {
-        internalType: "bytes32",
-        name: "s",
-        type: "bytes32"
+        "internalType": "uint256",
+        "name": "nounce",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "signature",
+        "type": "bytes"
       }
     ],
-    name: "claim",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function"
+    "name": "mint",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   }
 ]
 
-export class SOS extends Base {
+
+
+
+
+
+export class CMB extends Base {
   private _recipient: string
-  private _sosClaimContract: Contract;
-  private _sosERC20Contract: Contract;
+  private _cmbClaimContract: Contract;
+  private _cmbERC20Contract: Contract;
   private _sender: string;
 
-  constructor(provider: providers.JsonRpcProvider, sender: string, recipient: string, _sosTokenAddress: string) {
+
+  
+  constructor(provider: providers.JsonRpcProvider, sender: string, recipient: string, _cmbTokenAddress: string) {
     super()
     if (!isAddress(recipient)) throw new Error("Bad recipient Address")
     if (!isAddress(sender)) throw new Error("Bad sender Address")
     this._sender = sender;
     this._recipient = recipient;
-    this._sosClaimContract = new Contract(_sosTokenAddress, SOSCLAIM_ABI, provider);
-    this._sosERC20Contract = new Contract(_sosTokenAddress, ERC20_ABI, provider);
+    this._cmbClaimContract = new Contract(_cmbTokenAddress, cmbCLAIM_ABI, provider);
+    this._cmbERC20Contract = new Contract(_cmbTokenAddress, ERC20_ABI, provider);
   }
 
   async description(): Promise<string> {
-    return `claim SOS from ${this._sender}, and transfer to ${this._recipient}`
+    return `claim cmb from ${this._sender}, and transfer to ${this._recipient}`
   }
 
   async getSponsoredTransactions(): Promise<Array<TransactionRequest>> {
@@ -773,14 +106,16 @@ export class SOS extends Base {
     // const v = "12664759760331458874453076485325239921451404572804478121060178750865498554368"
     // const r = "0xe57fcd80afe2701a3469c0411080595ee77a8315481589b0494981f06b6e7cdd"
     // const s = "0x7a716d2e99812e791fd17afcbffe92c1980e25f68ea2519ec21d845ed672a7e8"
-    const total_share = ""
-    const v = ""
-    const r = ""
-    const s = ""
+
+    
+    const amount = "13000"
+    const nounce = "1"
+    const signature = "0xefd000836a1e79ec6d9382f6e4c22087cb6a8c67f8eccdc9e57cfe4db058d1b308bf2ffaae3df011156ed7c0f7623b2e4b00799aaddb2c52ae42771b524f7b731c"
+
 
     return [
-      { ...await this._sosClaimContract.populateTransaction.claim(v, r, s), gasLimit: 60000 },
-      { ...await this._sosERC20Contract.populateTransaction.transfer(this._recipient, parseUnits(total_share, 'ether')), gasLimit: 100000 }
+      { ...await this._cmbClaimContract.populateTransaction.mint(this._sender, amount, nounce, signature), gasLimit: 60000 },
+      { ...await this._cmbERC20Contract.populateTransaction.transferFrom(this._sender, this._recipient, amount), gasLimit: 100000 }
     ]
   }
 }
